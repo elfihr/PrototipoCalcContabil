eec19e7df7eba62c93a3f29c8c2cd84247b1eb4f
Luiz Felipe <70807899+elfihr@users.noreply.github.com>
2024-02-13 21:29:31 -0300

    teste mascara

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let calcular = document.querySelector('#calcular')
 let print = document.querySelector('#print')
 let selectTable = document.querySelector('#selectTable')
 let content_main = document.querySelector('#content_main')
-let recalcular = document.querySelector('#recalcular')
+
+
 
 let faixa01 = 0
 let faixa02 = 0
@@ -19,28 +20,29 @@ let tetoFaixa = 0
 let tetoInss = 0
 
 
+
+//====MascaraDinheiro
+
+function forMath() {   
+    formatMoney(salBruto);
+}
+function formatMoney(tempSalario){
+    var tempSalario;
+    if(tempSalario.value.length <= 0)tempSalario.value = "0,00";
+    var salTemp = tempSalario.value;
+    salTemp = salTemp + "";//Nao deixa dar um espaço(evita erros)
+    salTemp = parseInt (salTemp.replace(/[\D]+/g,""));//apenas numero
+    salTemp = salTemp+"";//Nao deixa dar um espaço(evita erros)
+    salTemp = salTemp.replace(/([0-9]{2})$/g,",$1");//Permite Virgula
+    tempSalario.value = salTemp
+};
+
+
+
+
  //====Funções
 
- function tabelaCalcInss(salBruto){
-    descFaixa01 = faixa01 * 0.075
-    descFaixa02 = (faixa02 - faixa01) * 0.09
-    descFaixa03 = (faixa03 - faixa02) * 0.12
-    descFaixa04 = (faixa04 - faixa03) * 0.14     
 
-    tetoFaixa = descFaixa01 + descFaixa02 + descFaixa03 + descFaixa04
-
-    if(salBruto < faixa01){
-        tetoInss = salBruto * 0.075                                            
-    }else if(salBruto < faixa02){
-        tetoInss =   ((salBruto - faixa01) * 0.09) + descFaixa01
-    }else if(salBruto < faixa03){
-        tetoInss =   ((salBruto - faixa02) * 0.12) + descFaixa02 + descFaixa01
-    }else if(salBruto < faixa04){
-        tetoInss =   ((salBruto - faixa03) * 0.14) + descFaixa03 + descFaixa02 + descFaixa01
-    }else if(salBruto > faixa04){
-        tetoInss =   tetoFaixa
-    }
- }
 const imprimir = n =>  print.innerHTML += `${n}<br>`
 const pulaLinha = () => print.innerHTML += `<br>`
 const real = n => n.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
@@ -53,16 +55,45 @@ function SBisValid(n){
     }
 }
 
+
 calcular.addEventListener('click', () => {
+    
+    print.innerHTML=''
 
-    print.innerHTML = '' 
+    let tempSalario = salBruto.value.replace(',','.')
+    let salarioBruto = Number(tempSalario)
+   
+    let inssBase = salarioBruto
+    tabelaCalcInss(salarioBruto)
 
-    calcular.style.display ='none'
-    recalcular.style.display='block'
+    console.log(typeof salarioBruto)
+   
+    console.log("salBruto.value: "+salBruto.value)
+    console.log("salarioBruto: "+salarioBruto)
+    console.log("inssBase: "+inssBase)
+    console.log("descFaixa01: "+descFaixa01)
+    console.log("-------------")
 
-    let salarioBruto = Number(salBruto.value)
-
-    let inssBase = Number(salBruto.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    function tabelaCalcInss(n){
+        descFaixa01 = faixa01 * 0.075
+        descFaixa02 = (faixa02 - faixa01) * 0.09
+        descFaixa03 = (faixa03 - faixa02) * 0.12
+        descFaixa04 = (faixa04 - faixa03) * 0.14     
+    
+        tetoFaixa = descFaixa01 + descFaixa02 + descFaixa03 + descFaixa04
+    
+        if(n < faixa01){
+            tetoInss = n * 0.075                                            
+        }else if(n < faixa02){
+            tetoInss =   ((n - faixa01) * 0.09) + descFaixa01
+        }else if(n < faixa03){
+            tetoInss =   ((n - faixa02) * 0.12) + descFaixa02 + descFaixa01
+        }else if(n < faixa04){
+            tetoInss =   ((n - faixa03) * 0.14) + descFaixa03 + descFaixa02 + descFaixa01
+        }else if(n > faixa04){
+            tetoInss =   tetoFaixa
+        }
+     }
     //==========Datas==========
     function tableDate(){
 if(selectTable.value == 2024){
@@ -74,14 +105,14 @@ if(selectTable.value == 2024){
     //====Faixa01
     function innsFaixa01(){
         imprimir("Periodo Calculado: " + selectTable.value)                       
-        imprimir("Valor Bruto Informado: " + inssBase) 
+        imprimir("Valor Bruto Informado: " + real(inssBase)) 
         let inssDesc = real(tetoInss)
         imprimir("Valor do INSS: "+ inssDesc) 
 
         //Memoria de Calculo
         pulaLinha()
         imprimir("Como foi calculado:") 
-        imprimir("Faixa 1: " + inssBase + " x 7.5%  + = " + inssDesc)
+        imprimir("Faixa 1: " + real(inssBase) + " x 7.5%  + = " + inssDesc)
         
         pulaLinha()
         imprimir("Total Contribuição INSS: " + inssDesc + " = ( " + inssDesc + " + 0,00 + 0,00 + 0,00)") 
@@ -89,7 +120,7 @@ if(selectTable.value == 2024){
      //====Faixa02
      function innsFaixa02(){
         imprimir("Periodo Calculado: " + selectTable.value)                        
-        imprimir("Valor Bruto Informado: " + inssBase) 
+        imprimir("Valor Bruto Informado: " + real(inssBase)) 
         let inssDesc = real(tetoInss)
         imprimir("Valor do INSS: "+ inssDesc) 
 
@@ -97,7 +128,7 @@ if(selectTable.value == 2024){
         pulaLinha()
         imprimir("Como foi calculado:") 
         imprimir("Faixa 1: " + real(faixa01) + " x 7.5% = "+ real(descFaixa01))
-        imprimir("Faixa 2: (" + inssBase + " - " + real(faixa01) + ") x 9% = " + real((salarioBruto - faixa01) * 0.09))
+        imprimir("Faixa 2: (" + real(inssBase) + " - " + real(faixa01) + ") x 9% = " + real((salarioBruto - faixa01) * 0.09))
 
         pulaLinha()
         imprimir("Total Contribuição INSS: " + real(inssDesc) + " = (" + real(descFaixa01) + " + " + real((salarioBruto - faixa01) * 0.09) + " + 0,00 + 0,00)")      
@@ -105,7 +136,7 @@ if(selectTable.value == 2024){
     //====Faixa03
     function innsFaixa03(){
         imprimir("Periodo Calculado: " + selectTable.value)                        
-        imprimir("Valor Bruto Informado: " + inssBase) 
+        imprimir("Valor Bruto Informado: " + real(inssBase)) 
         let inssDesc = real(tetoInss)
         imprimir("Valor do INSS: "+ inssDesc) 
 
@@ -122,7 +153,7 @@ if(selectTable.value == 2024){
     //====Faixa04
     function innsFaixa04(){
         imprimir("Periodo Calculado: " +selectTable.value) 
-        imprimir("Valor Bruto Informado: " + inssBase) 
+        imprimir("Valor Bruto Informado: " + real(inssBase)) 
         let inssDesc = real(tetoInss)
         imprimir("Valor do INSS: "+ inssDesc) 
 
@@ -139,7 +170,7 @@ if(selectTable.value == 2024){
     }
     function innsFaixaTeto(){
         imprimir("Periodo Calculado: " + selectTable.value) 
-        imprimir("Valor Bruto Informado: " + inssBase) 
+        imprimir("Valor Bruto Informado: " + real(inssBase)) 
         print.innerHTML += `Valor do INSS R$ ${tetoFaixa.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</br>` 
                 
         //Memoria de Calculo
@@ -153,47 +184,44 @@ if(selectTable.value == 2024){
         pulaLinha()
         imprimir("Total Contribuição INSS: " + real(tetoInss) + " = (" + real(descFaixa01) + " + " + real(descFaixa02) + " + " + real(descFaixa03) + " + " + real(descFaixa04) + ")") 
     }
-
-     if(SBisValid(salBruto.value)){
-    //=============InicioTabelaINSS=============
-        
-        //INSS 2024    
-
-
-            faixa01 = 1412
-            faixa02 = 2666.68
-            faixa03 = 4000.03
-            faixa04 = 7786.02
-
-            tabelaCalcInss(salarioBruto)
-
-            //faixa01 ate 1412
-            if(salarioBruto > 0 && salarioBruto <= faixa01){
-                innsFaixa01()
-            }                   
-            //faixa02 ate 2266.68
-            else if(salarioBruto > faixa01 && salarioBruto <= faixa02){
-                innsFaixa02()                   
-            }
-            //faixa03 ate 4000.03
-            else if(salBruto.value > faixa02 && salBruto.value <= faixa03){
-                innsFaixa03()
-            }
-            //faixa04 ate 7786.02
-            else if(salBruto.value > faixa03 && salBruto.value <= faixa04){
-                innsFaixa04()              
-            }
-            //faixaTeto depois 7786.02
-            else if(salBruto.value > faixa04){
-                innsFaixaTeto()
-            }                        
-        }                      
+    if(SBisValid(salarioBruto)){
+        //=============InicioTabelaINSS=============
+            
+            //INSS 2024    
+    
+    
+                faixa01 = 1412
+                faixa02 = 2666.68
+                faixa03 = 4000.03
+                faixa04 = 7786.02
+    
+                tabelaCalcInss(salarioBruto)
+                
+    
+                //faixa01 ate 1412
+                if(salarioBruto > 0 && salarioBruto <= faixa01){
+                    innsFaixa01()
+                }                   
+                //faixa02 ate 2266.68
+                else if(salarioBruto > faixa01 && salarioBruto <= faixa02){
+                    innsFaixa02()                   
+                }
+                //faixa03 ate 4000.03
+                else if(salarioBruto > faixa02 && salarioBruto <= faixa03){
+                    innsFaixa03()
+                }
+                //faixa04 ate 7786.02     
+                else if(salarioBruto > faixa03 && salarioBruto <= faixa04){
+                    innsFaixa04()              
+                }
+                //faixaTeto depois 7786.02
+                else if(salarioBruto > faixa04){
+                    innsFaixaTeto()
+                }                        
+            }                      
+                         
         //=============FimTabelaINSS=============
     }
 )
 
-recalcular.addEventListener('click', () => {
 
-        location.reload();
-
-})
